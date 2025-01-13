1b7b01a157e49f18edb6466311d2fda337de23a2
tanisha25 <tanishamedewala.25@gmail.com>
2025-01-14 03:40:56 +0530

    Sentimental Analysis using Reddit posts

--- start.sh
+++ start.sh
@@ -6,6 +6,5 @@ source .venv/bin/activate
 # Start Flask app using Gunicorn in the background
 gunicorn -w 4 -b 0.0.0.0:5001 app.app:app &
 
-# Start Streamlit app using the same port
-# Use subprocess to run Streamlit on the same port
-python -m streamlit run sentiment_app.py --server.port $PORT --server.headless true
+# Start Streamlit app using a different port (8502 in this case)
+python -m streamlit run sentiment_app.py --server.port 8502 --server.headless true
