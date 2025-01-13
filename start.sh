e7f270190cd96a569c141b1facb7c33dc42d9e84
tanisha25 <tanishamedewala.25@gmail.com>
2025-01-14 05:06:05 +0530

    Sentimental Analysis using Reddit posts

--- start.sh
+++ start.sh
@@ -13,8 +13,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 # Start Flask app using Gunicorn on the public Render port ($PORT)
-.venv/bin/gunicorn -w 4 -b 0.0.0.0:$PORT app.app:app &
-FLASK_PID=$!
-
-# Wait for the Flask process to exit
-wait $FLASK_PID
+exec gunicorn -w 4 -b 0.0.0.0:$PORT app:app
