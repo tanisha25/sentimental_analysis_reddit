da291eabbe47dcdbcfb56ac34cb4663fcaa987ab
tanisha25 <tanishamedewala.25@gmail.com>
2025-01-14 00:26:51 +0530

    Sentimental Analysis using Reddit posts

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 # Activate the virtual environment
 source .venv/bin/activate
 
-# Use the PORT provided by Render
-export FLASK_URL="http://0.0.0.0:$PORT"
-
 # Start Flask app using Gunicorn
-gunicorn -w 4 -b 0.0.0.0:$PORT app.app:app
+gunicorn -w 4 -b 0.0.0.0:8000 app.app:app &
+
+# Start Streamlit app
+python -m streamlit run streamlit.py --server.port $STREAMLIT_PORT --server.headless true
