94cd707bc5bd5c9b0151304728a4a4669258fc2d
tanisha25 <tanishamedewala.25@gmail.com>
2025-01-14 02:41:23 +0530

    Sentimental Analysis using Reddit posts

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from .sentiment.routes import sentiment_bp
 from .utils.config import Config
+from flask_cors import CORS
+
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    CORS(app)
 
     # Register Blueprints
     app.register_blueprint(sentiment_bp, url_prefix='/api/sentiment')
