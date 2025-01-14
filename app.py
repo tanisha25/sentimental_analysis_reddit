fbe52c6aec4311020e981804f2deffb4f3cb3778
tanisha25 <tanishamedewala.25@gmail.com>
2025-01-15 02:07:54 +0530

    Sentimental Analysis using Reddit Posts

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ if __name__ == '__main__':
     # Set up logging
     logger = configure_logger()
     port = int(os.getenv('PORT', 5000))  # Use the PORT from the environment, default to 5000 if not set
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
