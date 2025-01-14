c2eb066753c8aaf78e7261932d2b4634dc319d87
tanisha25 <tanishamedewala.25@gmail.com>
2025-01-15 04:01:14 +0530

    Sentimental Analysis using Reddit Posts

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ logger = configure_logger()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8080)
