1027d89c1c252b90f205429c422eca25f37306b2
tanisha25 <tanishamedewala.25@gmail.com>
2025-01-15 02:20:28 +0530

    Sentimental analysis using reddit posts

--- app.py
+++ app.py
@@ -6,14 +6,12 @@ from app.logger import configure_logger
 # Load environment variables
 load_dotenv()
 
+# Create the Flask app instance
+app = create_app()
 
+# Set up logging
+logger = configure_logger()
 
-# Check the environment and run the appropriate server
 if __name__ == '__main__':
-    # Create the Flask app instance
-    app = create_app()
-
-    # Set up logging
-    logger = configure_logger()
     port = int(os.getenv('PORT', 5000))  # Use the PORT from the environment, default to 5000 if not set
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=port)
