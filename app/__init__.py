e0e6fa6521ba9398dc81f0e4966ac786053a39dc
tanisha25 <tanishamedewala.25@gmail.com>
2025-01-15 04:56:04 +0530

    Sentimental Analysis using Reddit Posts

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from app.routes import sentiment_bp
 
 def create_app():
-  app = Flask(__name__, instance_relative_config=True)
+  app = Flask(__name__)
 
   # Enable CORS for all routes
   CORS(app)
